3c7188156c951424b1d97f907c7f837af2c08286
Rafay Ghafoor <rafayghafoor@gmail.com>
2017-09-15 13:13:20 +0500

    Download function added

--- subgrab/providers/subdb.py
+++ subgrab/providers/subdb.py
@@ -3,9 +3,8 @@ import hashlib
 import requests
 
 headers = {'User-agent': "SubDB/1.0 (subgrab/1.0; http://github.com/RafayGhafoor/Subscene-Subtitle-Grabber)"}
-languages = ['en', 'es', 'fr', 'it', 'nl', 'pl', 'pt', 'ro', 'sv', 'tr']
-search_url = "http://api.thesubdb.com/?action=search"
-download_url = "'http://api.thesubdb.com/?action=download"
+languages = ('en', 'es', 'fr', 'it', 'nl', 'pl', 'pt', 'ro', 'sv', 'tr')
+download_url = "http://api.thesubdb.com/?action=download"
 
 
 def get_hash(name):
@@ -17,5 +16,15 @@ def get_hash(name):
         data += f.read(readsize)
     return hashlib.md5(data).hexdigest()
 
-hash = get_hash("Doctor Strange 2016.mkv")
-r = requests.get(download_url + '&hash=')
+
+def download(hash, filename, language='en'):
+    r = requests.get(download_url + '&hash=' + hash + '&language=' + language, headers=headers)
+    with open("Dog.srt", 'wb') as f:
+        for chunk in r.iter_content(chunk_size=150):
+            if chunk:
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    hash = get_hash("A.Dogs.Purpose.2017.720p.WEB-DL.900MB.ShAaNiG.mkv")
+    s = download(hash=hash, filename="A.Dogs.Purpose.2017.720p.WEB-DL.900MB.ShAaNiG.mkv", language='en')
