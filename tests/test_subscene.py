6b7e50adb5316ed72214c03fa4c0a466b10f5153
Rafay Ghafoor <rafayghafoor@gmail.com>
2017-09-29 20:42:00 +0500

    Added test for sel_sub function (subscene.py)

--- tests/test_subscene.py
+++ tests/test_subscene.py
@@ -7,12 +7,16 @@ import subgrab.providers.subscene as subscene
 subscene.MODE = "silent"
 
 
-
 @pytest.mark.parametrize("name, expected", [
     ("Doctor Strange", 'https://subscene.com/subtitles/doctor-strange-2016/English'),
     ("The Intern", 'https://subscene.com/subtitles/the-intern-2015/English')
 ])
-
-
-def test_title(name, expected):
+def test_sel_title(name, expected):
     assert subscene.sel_title(name) == expected
+
+
+@pytest.mark.parametrize("url, expected", [
+    ("https://subscene.com/subtitles/release?q=pele.birth.of.the.legend", ['https://subscene.com/subtitles/pel-birth-of-a-legend/english/1336174']),
+])
+def test_sel_sub(url, expected):
+    assert subscene.sel_sub(url) == expected
