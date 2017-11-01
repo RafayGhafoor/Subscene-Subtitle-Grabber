88be4202f58e6d7c7236b3ac1dd1d926eb96cf02
Rafay Ghafoor <rafayghafoor@gmail.com>
2017-11-01 20:51:12 +0500

    Exception replaced with condition

--- tests/test_titleparser.py
+++ tests/test_titleparser.py
@@ -1,22 +1,23 @@
 import re
-import pytest
+import unittest
 
+input_names = ['Doctor.Strange.2016.HEVC.1080p.mp4',
+'The Intern  2015 Bluray.mp4',
+'Spider-Man.Homecoming.2017.1080p.AMZN.WEB-DL.DD.5.1.H.264-SiGMA.avi'
+]
 
-teststr = ["Doctor Strange 2016", "Doctor-Strange", "Doctor-Strange-2016"]
+expected_names = ['Doctor Strange 2016',
+'The Intern 2015',
+'Spider Man Homecoming 2017'
+]
 
 def title_parser(name):
-	if re.search(r'\d{4}', name):
-		name, _, removal = name.partition(re.search(r'\d{4}', name).group())
-		return name.strip()
-	else:
-		return name
+	year_regex = re.compile(r'(?!1080|2160)p?\d{4}', 'abc 2016')
+	removals = set(re.findall('\W+', name))
+	for chars in removals:
+		name = name.replace(chars, ' ')
+	return name
 
-
-@pytest.mark.parametrize("name, answer", [
-    (teststr[0], 'Doctor Strange'),
-    (teststr[1], 'Doctor-Strange'),
-    (teststr[2], 'Doctor-Strange'),
-])
-
-def test_answer(name, answer):
-    assert title_parser(name) == answer
+class TitleParserTest(unittest.TestCase):
+	def test_titles(self):
+		pass
